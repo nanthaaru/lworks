f9db74a29e906c876add436bdb99c9695d1688c2
nantha <nanthakumar.a@gmail.com>
2015-06-21 11:13:35 +0530

    fixing verification email code

--- features/step_definitions/aircraft_steps.rb
+++ features/step_definitions/aircraft_steps.rb
@@ -10,7 +10,7 @@ Given(/^user logins and navigates to home page$/) do
   page.fill_in 'username', :with => 'nantha.qa@lw.com'
   page.fill_in 'password', :with => 'lworks123'
   page.click_button 'Login'
-  if page.find('input', :title => "Email me a verification code" ) != nil
+  if page.find('input', :text => "Email me a verification code" ) != nil
     page.click_button 'save'
     sleep 15
     verification_code = reademail
